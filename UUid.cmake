4ffc29f412f6b6c01e33ea0aee530cc76d7031b6
Daniil A Megrabyan <d.megrabyan@cadabra.systems>
2025-02-04 15:10:16 +0300

    Linux specific #build

--- UUid.cmake
+++ UUid.cmake
@@ -3,6 +3,7 @@ cmake_minimum_required(VERSION 3.12.4)
 include_guard(GLOBAL)
 
 if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+	find_package(PkgConfig REQUIRED)
 	pkg_search_module(PC_UUID REQUIRED uuid)
 	find_path(UUID_INCLUDE_DIRS NAMES uuid/uuid.h HINTS ${PC_UUID_INCLUDEDIR} ${PC_UUID_INCLUDE_DIRS} PATH_SUFFIXES uuid)
 	find_library(UUID_LIBRARIES NAMES uuid HINTS ${PC_UUID_DIR} ${PC_UUID_LIBRARY_DIRS})
