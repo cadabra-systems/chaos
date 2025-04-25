24cbf5971847cb8b03ba2a723c51b96de7f543e4
Daniil A Megrabyan <d.megrabyan@cadabra.systems>
2025-04-25 21:16:58 +0300

    Naive cache implementation moved from CAP #feature

---
+++ Memento.cpp
@@ -0,0 +1,115 @@
+/**
+ @file Memento.cpp
+ @date 09.03.17
+ @copyright Cadabra / Ace
+ @author Daniil A Megrabyan
+*/
+
+#include "Memento.hpp"
+
+namespace chaos {
+	memento::object::object(object_type type)
+	:
+		_type(object_type::null),
+		_null(nullptr)
+	{
+		set_type(type);
+	}
+
+	memento::object_type memento::object::get_type() const
+	{
+		return _type;
+	}
+
+	std::string* memento::object::get_string() const
+	{
+		return object_type::string == _type ? _string : nullptr;
+	}
+
+	std::vector<std::string>* memento::object::get_vector() const
+	{
+		return object_type::list == _type ? _list : nullptr;
+	}
+
+	std::set<std::string>* memento::object::get_set() const
+	{
+		return object_type::set == _type ? _set : nullptr;
+	}
+
+	std::map<std::string, std::string>* memento::object::get_map() const
+	{
+		return object_type::map == _type ? _map : nullptr;
+	}
+
+	memento::object_type memento::object::set_type(object_type type)
+	{
+		object_type retval(object_type::null);
+		switch (_type) {
+			case object_type::string:
+				retval = object_type::string;
+				delete _string;
+				break;
+			case object_type::list:
+				retval = object_type::list;
+				delete _list;
+				break;
+			case object_type::set:
+				retval = object_type::set;
+				delete _set;
+				break;
+			case object_type::map:
+				retval = object_type::map;
+				delete _map;
+				break;
+			default:
+				retval = object_type::null;
+				break;
+		}
+		switch (type) {
+			case object_type::string:
+				_string = new std::string();
+				break;
+			case object_type::list:
+				_list = new std::vector<std::string>();
+				break;
+			case object_type::set:
+				_set = new std::set<std::string>();
+				break;
+			case object_type::map:
+				_map = new std::map<std::string, std::string>();
+				break;
+			default:
+				_null = nullptr;
+				break;
+		}
+		_type = type;
+		return retval;
+	}
+
+	bool memento::reset(const std::string& key)
+	{
+		return _collection.erase(std::hash<std::string>()(key)) == 1;
+	}
+
+	chaos::safe_ptr<memento::object> memento::get(const std::string& key) const
+	{
+		memento::collection::const_iterator o(_collection.find(std::hash<std::string>()(key)));
+		return _collection.cend() == o ? nullptr :  o->second;
+	}
+
+	chaos::safe_ptr<memento::object> memento::set(const std::string& key, object_type type)
+	{
+		return _collection.insert(std::hash<std::string>()(key), chaos::make_safe<memento::object>(type), true).first->second;
+	}
+
+	bool memento::is_exists(const std::string& key) const
+	{
+		return _collection.cend() != _collection.find(std::hash<std::string>()(key));
+	}
+
+	bool memento::is_null(const std::string& key) const
+	{
+		memento::collection::const_iterator o(_collection.find(std::hash<std::string>()(key)));
+		return _collection.cend() == o || o->second->get_type() == object_type::null;
+	}
+}
---
+++ Memento.hpp
@@ -0,0 +1,108 @@
+/**
+ @file Memento.hpp
+ @date 09.03.17
+ @copyright Cadabra / Ace
+ @author Daniil A Megrabyan
+*/
+
+#ifndef Chaos_Money_hpp
+#define Chaos_Money_hpp
+
+#include "SafePointer.hpp"
+#include "AtomicHashTable.hpp"
+
+#include <map>
+#include <set>
+#include <vector>
+#include <string>
+
+namespace chaos {
+	class memento
+	{
+	/** @name Classes */
+	/** @{ */
+	public:
+		enum class object_type : char
+		{
+			null = 0,
+			string = 's',
+			list = 'L',
+			set = 'S',
+			map = 'M',
+		};
+
+		class object
+		{
+		public:
+			object(object_type type = object_type::null);
+			~object() = default;
+
+		private:
+			object_type _type;
+			union {
+				void* _null;
+				std::string* _string;
+				std::vector<std::string>* _list;
+				std::set<std::string>* _set;
+				std::map<std::string, std::string>* _map;
+			};
+
+		public:
+			object_type get_type() const;
+			std::string* get_string() const;
+			std::vector<std::string>* get_vector() const;
+			std::set<std::string>* get_set() const;
+			std::map<std::string, std::string>* get_map() const;
+
+		public:
+			object_type set_type(object_type type);
+		};
+	/** @} */
+
+	/** @name Aliases */
+	/** @{ */
+	public:
+		using collection = chaos::atomic_hash_table<chaos::safe_ptr<object>>;
+	/** @} */
+
+	/** @name Constructors */
+	/** @{ */
+	public:
+		memento() = default;
+		~memento() = default;
+	/** @} */
+
+	/** @name Properties */
+	/** @{ */
+	private:
+		collection _collection;
+	/** @} */
+
+	/** @name Procedures */
+	/** @{ */
+	public:
+		bool reset(const std::string& key);
+	/** @} */
+
+	/** @name Getters */
+	/** @{ */
+	public:
+		chaos::safe_ptr<object> get(const std::string& key) const;
+	/** @} */
+
+	/** @name Setters */
+	/** @{ */
+	public:
+		chaos::safe_ptr<object> set(const std::string& key, object_type type);
+	/** @} */
+
+	/** @name States */
+	/** @{ */
+	public:
+		bool is_exists(const std::string& key) const;
+		bool is_null(const std::string& key) const;
+	/** @} */
+	};
+}
+
+#endif
