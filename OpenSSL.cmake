06eda8836b0045b204017a0ae95b29b5c7bb3ad7
Daniil A Megrabyan <d.megrabyan@cadabra.systems>
2025-07-18 11:25:19 +0300

    OpenSSL #upgrade from 1 to 3

--- OpenSSL.cmake
+++ OpenSSL.cmake
@@ -3,8 +3,8 @@ cmake_minimum_required(VERSION 3.12.4)
 include_guard(GLOBAL)
 
 if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-	set(OPENSSL_ROOT_DIR "${BREW_HOME}/openssl@1.1/lib")
-	set(OPENSSL_INCLUDE_DIR "${BREW_HOME}/openssl@1.1/include")
+	set(OPENSSL_ROOT_DIR "${BREW_HOME}/openssl/lib")
+	set(OPENSSL_INCLUDE_DIR "${BREW_HOME}/openssl/include")
 	message(STATUS "OpenSSL was detected at ${OPENSSL_ROOT_DIR} and ${OPENSSL_INCLUDE_DIR}")
 endif ()
 
